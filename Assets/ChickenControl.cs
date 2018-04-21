9ca25b755076cf386c47439464f3e069d87ca6c2
Alberto <albertoesbueno@hotmail.com>
2018-04-21 12:56:53 +0100

    TextureScroller fail

--- Assets/ChickenControl.cs
+++ Assets/ChickenControl.cs
@@ -18,7 +18,7 @@ public class ChickenControl : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		Vector2 move = Vector3.zero;
 		if(Input.GetKey(up)) move += Vector2.up;
 		if(Input.GetKey(down)) move += Vector2.down;
