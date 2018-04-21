7dcd6765af7dbfdb87c9b65002213d8dd16a1005
Alberto <albertoesbueno@hotmail.com>
2018-04-21 11:29:40 +0100

    Scripts de movimiento

---
+++ Assets/ChickenChair.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenChair : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Move(Vector2 movement) {
+		rb.velocity = movement;
+	}
+}
---
+++ Assets/ChickenControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChickenControl : MonoBehaviour {
+
+	[SerializeField] private KeyCode up;
+	[SerializeField] private KeyCode down;
+	[SerializeField] private KeyCode left;
+	[SerializeField] private KeyCode right;
+
+	private ChickenChair chair;
+
+	// Use this for initialization
+	void Start () {
+		chair = GetComponent<ChickenChair>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 move = Vector3.zero;
+		if(Input.GetKeyDown(up)) move += Vector2.up;
+		if(Input.GetKeyDown(down)) move += Vector2.down;
+		if(Input.GetKeyDown(right)) move += Vector2.right;
+		if(Input.GetKeyDown(left)) move += Vector2.left;
+
+		chair.Move(move);
+	}
+}
