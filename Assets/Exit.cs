e1a38e329dfea6cafb63c316357cf3d6bf5ed7a5
yacoanix <aymedeyacoan@gmail.com>
2018-04-21 20:11:04 +0100

    no me des errores, subida de menu

---
+++ Assets/Exit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
---
+++ Assets/Start.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour {
+
+    public void LoadByIndex(string sceneIndex)
+    {
+        SceneManager.LoadScene (sceneIndex);
+    }
+}
