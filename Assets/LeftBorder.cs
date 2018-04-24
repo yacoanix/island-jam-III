6bd9fc73cbe85dfc00dd97eb9c9ee906eb8a1b39
Alberto <albertoesbueno@hotmail.com>
2018-04-24 19:25:51 +0100

    Últimos cambios no commiteados

---
+++ Assets/LeftBorder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftBorder : MonoBehaviour {
+
+	float width;
+
+	// Use this for initialization
+	void Start () {
+		width = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetPositionRelativeToCamera();
+	}
+
+	void SetPositionRelativeToCamera() {
+		Vector2 cameraBounds = Camera.main.GetComponent<BackgroundScroll>().CameraBounds();
+		
+		transform.position = new Vector3(
+			cameraBounds.x + width / 2.0f,
+			transform.position.y,
+			transform.position.z
+		);
+	}
+}
---
+++ Assets/MenuSong.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSong : MonoBehaviour {
+
+	private static bool created = false;
+
+    void Start()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+
+    public void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "race")
+        {
+            Destroy(gameObject);
+			created = false;
+        }
+    }
+
+}
