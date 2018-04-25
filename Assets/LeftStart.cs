5ee744924cf23110f773867615a9a83f5bca8bbd
Alberto <albertoesbueno@hotmail.com>
2018-04-25 18:06:40 +0100

    Resuelto bug del modo ventana

---
+++ Assets/LeftStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftStart : MonoBehaviour {
+
+	private float offset = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		Vector2 cameraBounds = Camera.main.GetComponent<BackgroundScroll>().CameraBounds();
+		
+		transform.position = new Vector3(
+			cameraBounds.x + offset,
+			transform.position.y,
+			transform.position.z
+		);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
